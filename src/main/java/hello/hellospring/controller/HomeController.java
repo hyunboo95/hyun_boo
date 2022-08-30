615f51ca18321e441c8d3a194579157fbd02736b
hyunboo95 <80379893+hyunboo95@users.noreply.github.com>
2022-08-30 22:59:37 +0900

    createMemberForm

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -2,7 +2,6 @@ package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,10 +12,7 @@ public class HomeController {
         return "home"; //home.html이 호출이 된다.
     }
 
-    @GetMapping(value="/members/new")
-    public String createForm() {
-        return "members/createMemberForm";
-    }
+    
     
     
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,7 +2,10 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller //컨트롤러라는 애노테이션을 보고, 스프링이 뜰 때, MemberController라는 객체를 생성해서 가지고 있다.
@@ -16,6 +19,20 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping(value = "/members/new") // Get방식 : url을 직접 입력하는 방식 :: 조회할 때 주로 사용
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new") // Post 방식 : 데이터를 전달할 때 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        
+        memberService.join(member);
+        return "redirect:/";
+}
 
     
     
